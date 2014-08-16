8be24e0a6d342db9e5e1ba0d5897a40b7a7b2f53
Franz Schnyder <schnyderfranz@gmail.com>
2014-08-16 11:20:48 +0200

    Allow setting of days and averaging for graphs

--- projects/iot/sensors_overview.js
+++ projects/iot/sensors_overview.js
@@ -36,7 +36,15 @@ function Sensor(id) {
 function ViewModel() {
     var self = this;
 
-    self.graphs = [ { id : "15134" }, { id : "15135" }, { id : "15136" }  ]
+    self.graphs = [ { id : "15134" }, { id : "15135" }, { id : "15136" }  ];
+
+
+
+    self.days = ko.observable("1");
+
+    self.averages = ["0", "10", "15", "20", "30", "60", "240", "720", "1440", "daily"]
+    self.average = ko.observable("10");
+
 
     self.sensors = ko.observableArray();
 
